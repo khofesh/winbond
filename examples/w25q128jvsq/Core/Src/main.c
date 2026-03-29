281802790194f865d970ac645dd955f791749c4b
fahmi ahmad <fahmiahmad@tutanota.com>
2026-03-29 12:12:50 +0700

    remove duplicate

--- examples/w25q128jvsq/Core/Src/main.c
+++ examples/w25q128jvsq/Core/Src/main.c
@@ -181,17 +181,6 @@ int main(void)
 
   /* USER CODE END 2 */
 
-  /* Initialize COM1 port (115200, 8 bits (7-bit data + 1 stop bit), no parity */
-  BspCOMInit.BaudRate   = 115200;
-  BspCOMInit.WordLength = COM_WORDLENGTH_8B;
-  BspCOMInit.StopBits   = COM_STOPBITS_1;
-  BspCOMInit.Parity     = COM_PARITY_NONE;
-  BspCOMInit.HwFlowCtl  = COM_HWCONTROL_NONE;
-  if (BSP_COM_Init(COM1, &BspCOMInit) != BSP_ERROR_NONE)
-  {
-    Error_Handler();
-  }
-
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
